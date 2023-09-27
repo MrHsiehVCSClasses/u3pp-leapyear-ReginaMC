b9d527d706530f409349568ae921507b8ea14019
Regina <regic.2009@gmail.com>
2023-09-27 15:23:26 -0700

    Changes to u3pp-leapyear

--- src/main/java/u3pp/LeapYear.java
+++ src/main/java/u3pp/LeapYear.java
@@ -1,17 +1,29 @@
 package u3pp;
 
 /**
- * YOUR COMMENTs HERE
+ * the LeapYear class determines whether a year is a leap year or not with the method isLeapYear
  * 
- * @author //YOUR NAME HERE
+ * @author //Regina
  *
  */
 
 public class LeapYear {
 	/**
-	 * YOUR COMMENTS HERE
+	 * if the year is less than 1582 then it returns false
+	 * if the year is divisible by 4, not 100, and not 400, it returns true 
+	 * if it is divisible by 4, not 100, and 400, it returns true
+	 * otherwise it returns false
 	 */
 	public static boolean isLeapYear(int year) {
-		/* YOUR CODE HERE */
+		if (year < 1582) {
+			return false; 
+		}
+		else if ((year % 4 == 0) && !(year % 100 == 0)) {
+			return true;
+		}
+		else if ((year % 400 == 0)) {
+			return true;
+		}
+		else { return false;}
 	}
 }
--- src/main/java/u3pp/Main.java
+++ src/main/java/u3pp/Main.java
@@ -3,25 +3,37 @@ package u3pp;
 import java.util.Scanner;
 
 /**
- * YOUR COMMENTS HERE
+ * the Main class uses a scanner to take in input by the user
+ * the input is converted to an integer
+ * with an inputed year, the method isLeapYear determines whether or not 
+ * and prints the year and if it is nor isn't a leap year
+ * the user can stop the input when STOP is the input
  * 
- * @author YOUR NAME HERE
+ * @author Regina
  */
 
 class Main {
 
 	public static void main(String[] args) {
 
-		//YOUR CODE HERE
+		Scanner sc = new Scanner (System.in);
 
 		System.out.print("Enter a year (or enter STOP to end): ");
-		String input = sc.nextLine();
+		String input = sc .nextLine();
 
 		while (!input.toUpperCase().equals("STOP")) {
-			/* YOUR CODE HERE */
+			/* 
+			 * testYear becomes an integer and if isLeapYear is true then it is a 
+			 * leapyear and if not then it is not a leapyear
+			 */
 			System.out.print("Enter a year (or enter STOP to end): ");
-			//YOUR CODE HERE
-		}
-		//YOUR CODE HERE
+			int testYear = Integer.parseInt(input);
+			if (LeapYear.isLeapYear(testYear)== true) {
+				System.out.println(testYear + " is a leap year");}
+			else {
+				System.out.println(testYear + " is not a leap year");}
+			input = sc.nextLine();}
+		sc.close();
+		
 	}
 }
